fa8abfd7fa6f25f3cd31111923bf8bd2a5344efd
madao-madao <mamamatao@yandex.ru>
2024-08-08 21:22:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<head>
+  <title>Главная</title> <!-- Заголовок страницы -->
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <!-- Таблица символов RU -->
+</head>
+
+<body>
+  <nav> <!-- nav-navigation(по сайту)-->
+    <a href="/">Главная</a> <a href="/profile">Профиль</a>
+  </nav>
+  <div><!-- Блоки для разделения контента-->
+    <h3>Лучшие шутеры в истории</h3> <!-- H заголовок третьего уровня -->
+    <ol> <!--устанавливает нумерованный список-->
+      <li>Стефен Карри <br> <img width="100px" src="/assets\images\SC.jpg
+      "></li> <!-- отдельный элемент списка-->
+      <li>Рэй Аллен <br><img width="100px" src="/assets\images\RA.jpg
+      "></li>
+      <li>Джеймс Харден <br> <img width="100px" src="/assets\images\JH.jpg
+      "></li>
+      <li>Дамиан Лиллард <br> <img width="100px" src="/assets\images\DM.jpg
+      "></li>
+      <li>Реджи Миллер <br> <img width="100px" src="/assets\images\RA.jpg
+      "></li>
+    </ol>
+  </div>
+  <div>
+    <h3>Хайлайты</h3>
+    <video controls="controls" loop="loop">
+      <source src="/assets\videos\highlight.webm">
+    </video>
+  </div>
+
+</body>
